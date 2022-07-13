9b6d423cddcd4c37a936f6470c6de79041e9687a
shallyO <shalomogbeche@gmail.com>
2022-07-13 23:57:24 +0100

    pages adjusted

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import WithdrawView from '../views/WithdrawView.vue'
 import TransferView from '../views/TransferView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL), 
   routes: [
     {
       path: '/',
